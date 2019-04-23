6acd95614536fe4dc0d3fa7679a70e6262df0295
ashwaniK <ashwaniK@AshwaniK-VM>
2019-04-23 22:46:54 +0530

    updated DBServiceResource!

--- src/main/java/com/microservices/stock/dbservice/resource/DBServiceResource.java
+++ src/main/java/com/microservices/stock/dbservice/resource/DBServiceResource.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +16,24 @@ import com.microservices.stock.dbservice.repository.QuotesRepository;
 @RestController
 @RequestMapping("/rest/db")
 public class DBServiceResource {
+
 	private QuotesRepository quotesRepository;
+
+	public DBServiceResource(QuotesRepository quotesRepository) {
+		this.quotesRepository = quotesRepository;
+	}
 
 	@GetMapping("/{username}")
 	public List<String> getQuotes(@PathVariable("username") final String username) {
 		return quotesRepository.findByUserName(username).stream().map(Quotes::getQuote).collect(Collectors.toList());
 
 	}
+
+	@PostMapping("/add")
+	public List<String> add(@RequestBody final Quotes quotes) {
+
+		return null;
+
+	}
+
 }
